b0b24ab0886672a7ee2ceff4596b4be67aba5377
tiphergane <tiphergane@gmail.com>
2018-10-01 17:01:57 +0200

    Smb1Mount v3.0
    better use of functions and add of sleep timer to read console output

--- MountSMBv1.ps1
+++ MountSMBv1.ps1
@@ -1,36 +1,61 @@
 ﻿######################################################################
 # Script monter les points de montages pour GitHub
-# version: 2.1
+# version: 3.0
 # Auteur: tiphergane/meoowrwa
 # Synopsis: Pour vérifier si le protocol SMBv1 est activé ou non, et modifier la configuration le cas échéant.
 # Usage: ajoutez les points de montages un par un dans la foncion MountDir. Il faut utiliser le mode administrateur obligatoirement
 # Si vous ne voulez ne plus avoir la question sur l'execution, dans un powershell elevé (en mode admin) tapez: Set-ExecutionPolicy Unrestricted
 ######################################################################
 
-Import-Module SmbShare
+function ActivateShare {
 
-Get-SmbServerConfiguration | findstr /i EnableSMB1protocol | findstr /i false | Out-Null
+		Get-Module SmbShare | findstr /i smbshare | Out-Null
+		if ($LASTEXITCODE -eq 0)
+			{
+				Import-Module SmbShare
+			}
+		else
+			{
+				#Si le test montre que SMBv1 est activé
+				Write-Output "Module déjà chargé, on passe à la suite"
+				start-sleep 5
+			}
+}
+
+function TestSmb {
+
+		Write-Output "Activation du protocol SMB v1, un reboot est nécessaire si le protocol n'est pas chargé" 
+		Get-SmbServerConfiguration | findstr /i EnableSMB1protocol | findstr /i false | Out-Null
+	
+			if ($LASTEXITCODE -eq 0)
+			{
+				#Si le test montre que SMBv1 est désactivé
+				ActivateSMB
+				Write-Output "Reboot nécessaire" 
+				start-sleep 5
+			}
+			else
+			{
+				#Si le test montre que SMBv1 est activé
+				MountDir
+				start-sleep 5
+			}
+
+}
 
 function ActivateSMB {
         Write-Output "Activation du protocol SMB v1" 	
         Enable-WindowsOptionalFeature -Online -FeatureName smb1protocol
-    }
+}
 
 function MountDir {
-            Write-Output "Montage du repertoire dir$"
-            New-SmbMapping -LocalPath "B:" -RemotePath "\\srv\dir$"
+            Write-Output "Montage du repertoire SRV1$"
+            New-SmbMapping -LocalPath B: -RemotePath \\SRV1\FOLDER$
+			start-sleep 5
+			Write-Output "Montage du repertoire SRV2"
+			New-SmbMapping -LocalPath M: -RemotePath \\SRV2\FOLDER2\FOLDER2.l
             #Ajoute tes autres points de montage ici
-    }
-
-
-if ($LASTEXITCODE -eq 0)
-{
-    #Si le test montre que SMBv1 est désactivé
-    ActivateSMB
-    MountDir
 }
-else
-{
-    #Si le test montre que SMBv1 est activé
-    MountDir
-}
+
+ActivateShare
+TestSmb
